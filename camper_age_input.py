7d234d8b358a337e59bd7a8cafd1e43729248801
edhiatt1 <70347869+edhiatt1@users.noreply.github.com>
2020-09-07 21:20:06 -0500

    Add files via upload

---
+++ camper_age_input.py
@@ -0,0 +1,12 @@
+age_in_months = int(input("How many months old do you want to convert into years?"))
+age_in_years = age_in_months / 12
+print("Number of years is", age_in_years)
+
+'''
+I could not understand the writton instructions. I really struggled with this and lost motivation. 
+The help form was'nt even in this module and I would like to talk about this assignment to help me
+better understand it. 
+'''
+
+
+
---
+++ test_functions.py
@@ -0,0 +1,15 @@
+import unittest
+from main import camper_age_input
+def convert_to_months(x):
+    pass
+
+
+if __name__ == '__main__':
+
+class FunctionTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(False, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
